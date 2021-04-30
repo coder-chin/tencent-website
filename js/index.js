0dce028ab23856d40064e35b7b2707d67e32f396
qhy <1277180540@qq.com>
2021-04-30 17:52:59 +0800

    try

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
 const link = document.getElementById('link');
+const details = document.querySelectorAll('.details-area .details .item');
+
+details[0].addEventListener('mouseover',function(){
+  details[0].classList.remove('intro');
+  details[0].classList.add('intro_hover');
+})
+details[1].addEventListener('mouseover',function(){
+  details[1].classList.remove('culture');
+  details[1].classList.add('culture_hover');
+})
+details[2].addEventListener('mouseover',function(){
+  details[2].classList.remove('location');
+  details[2].classList.add('location_hover');
+})
+
+details[0].addEventListener('mouseout', function () {
+  details[0].classList.add('intro');
+  details[0].classList.remove('intro_hover');
+})
+details[1].addEventListener('mouseout', function () {
+  details[1].classList.add('culture');
+  details[1].classList.remove('culture_hover');
+})
+details[2].addEventListener('mouseout', function () {
+  details[2].classList.add('location');
+  details[2].classList.remove('location_hover');
+})
 
 function timer(){
   var top = -243;
