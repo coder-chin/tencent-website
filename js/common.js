a628ec47af7252d47fa89760d3c998d506def77a
qhy <1277180540@qq.com>
2021-05-22 23:26:22 +0800

    5.22

--- js/common.js
+++ js/common.js
@@ -1,16 +1,16 @@
 const headNav = document.querySelector(".header .header-nav .nav-list");
 const headcore = document.querySelector(".header .header-nav");
 const header = document.getElementsByClassName('header')[0];
-const logo = document.getElementsByClassName('logo')[0];
+const logo = document.getElementsByClassName('logo_word')[0];
 const as = document.querySelectorAll('.header .header-nav .nav-list li a');
 const lis = document.querySelectorAll('.header .header-nav .nav-list > li');
 const mores = document.querySelectorAll('.header .header-nav .nav-list li .more-list');
 const langs = document.querySelectorAll('.header .header-nav .lang a,i');
 
 
-//鼠标移到顶部导航栏发生的事件，和滚轮向上滚动发生的事件
+//鼠标移到顶部导航栏发生的事件，和滚轮向上滚动发生的事件函数
 let headerChange = function(){
-  logo.classList.add('logo_hover');
+  logo.classList.add('logo_a');   //logo字体变化
   as.forEach(element => {
     element.classList.add('head-color');
   });
@@ -19,10 +19,10 @@ let headerChange = function(){
   });
 }
 
-//鼠标向下滚动和离开顶部导航栏
+//鼠标向下滚动和离开顶部导航栏的事件函数
 let headerBack = function(){
   header.style.height = "0";
-  logo.classList.remove('logo_hover');
+  logo.classList.remove('logo_a');
   as.forEach(element => {
     element.classList.remove('head-color');
   });
@@ -31,29 +31,7 @@ let headerBack = function(){
   });
 }
 
-//鼠标放到顶部导航
-headNav.onmouseover = function() {
-  header.style.height = "442px";
-  header.style.backgroundColor = '#fff';
-  headerChange();
-}
-//鼠标离开顶部导航
-headNav.onmouseout = function() {
-  headerBack();
-}
-
-//监听顶部导航每个item （控制二级菜单的展开和隐藏）
-for (let index = 0; index < lis.length; index++) {
-  const element = lis[index];
-  element.addEventListener('mouseover',function(){
-    mores[index].style.display = 'block';
-  })
-  element.addEventListener('mouseout',function(){
-    mores[index].style.display = 'none';
-  })
-}
-
-//监听滚轮事件
+//监听滚轮事件函数
 var scrollFunc = function (e) {
   if (e.wheelDelta) {
     if (e.wheelDelta > 0 ) {     //当鼠标滚轮向上滚动时
@@ -75,5 +53,30 @@ var scrollFunc = function (e) {
   } 
 }
 
-window.addEventListener("wheel", scrollFunc)
+
+//鼠标放到顶部导航触发事件
+headNav.onmouseover = function() {
+  header.style.height = "442px";
+  header.style.backgroundColor = '#fff';
+  headerChange();
+}
+//鼠标离开顶部导航触发事件
+headNav.onmouseout = function() {
+  headerBack();
+}
+
+//监听顶部导航每个item （控制二级菜单的展开和隐藏）
+for (let index = 0; index < lis.length; index++) {
+  const element = lis[index];
+  element.addEventListener('mouseover',function(){
+    mores[index].style.display = 'block';
+  })
+  element.addEventListener('mouseout',function(){
+    mores[index].style.display = 'none';
+  })
+}
+
+
+
+window.addEventListener("wheel", scrollFunc)    //window监听
 
